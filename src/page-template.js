d160e42500c2df8ae3edcb04d62678708d112046
unknown <ericbwebdev86@gmail.com>
2022-02-04 10:37:52 -0600

    added font awesome cdn and icons for roles

--- src/page-template.js
+++ src/page-template.js
@@ -5,7 +5,7 @@ const buildManagerCard = function (manager) {
                 style="width: 18rem; box-shadow: 0 8px 6px -6px black;">
                 <div class="card-body">
                     <h4 class="card-title">${manager.name}</h4>
-                    <h5 class="card-title">Manager</h5>
+                    <h5 class="card-title"><i class="fas fa-mug-hot"></i> Manager</h5>
                 </div>
                 <div class="container-fluid bg-secondary pt-5 pb-5">
                     <ul class="list-group list-group-flush">
@@ -25,7 +25,7 @@ const buildEngineerCard = function (engineer) {
 
                 <div class="card-body">
                     <h5 class="card-title">${engineer.name}</h5>
-                    <h5 class="card-title">Engineer</h5>
+                    <h5 class="card-title"><i class="fas fa-glasses"></i> Engineer</h5>
                 </div>
                 <div class="container-fluid bg-secondary pt-5 pb-5">
                     <ul class="list-group list-group-flush">
@@ -44,7 +44,7 @@ const buildInternCard = function (intern) {
                 style="width: 18rem; box-shadow: 0 8px 6px -6px black;">
                 <div class="card-body">
                     <h4 class="card-title">${intern.name}</h4>
-                    <h5 class="card-title">Intern</h5>
+                    <h5 class="card-title"><i class="fas fa-user-graduate"></i> Intern</h5>
                 </div>
                 <div class="container-fluid bg-secondary pt-5 pb-5">
                     <ul class="list-group list-group-flush">
@@ -100,6 +100,7 @@ const HTMLtemplate = function (cardDeck) {
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <title>Org Chart Builder</title>
 </head>
 
